25d60ee017a72c9220886ef557510785e24448fc
Richard Hawkins <Richard.Hawkins@Revature.com>
2026-03-10 12:59:35 -0500

    update with daily challenges and chinook

---
+++ W3/training_db.sql
@@ -0,0 +1,7 @@
+INSERT INTO associates 
+(first_name, last_name,  email, hire_date)
+VALUES 
+('Dylan', 'Parrott', 'dylan.parrot@revature.net', '2026-02-23'),
+('Lee', 'Wilson', 'lee.wilson@revature.net', DEFAULT);
+
+SELECT * FROM associates;
