f6aae07f86b7087a7129da588a71f8020356fcb1
Richard Hawkins <Richard.Hawkins@Revature.com>
2026-04-13 14:04:22 -0500

    reconnect to a deployed model

---
+++ W8/Demos/Deployed-Model.py
@@ -0,0 +1,59 @@
+# could run http reqests - anything can use http
+# could run boto3 to talk to aws - al little more fundamental
+# could make a predictor (sagemake SDK - python - heavyweight package)
+
+import boto3
+import json
+import sagemaker
+import time
+from sagemaker.predictor import Predictor
+from sagemaker.serializers import JSONSerializer
+from sagemaker.deserializers import JSONDeserializer
+
+def prediction_with_sagemaker(input_data, endpoint_name):
+    print("Predicting with SageMaker SDK...")
+    predictor = Predictor(
+        endpoint_name=endpoint_name,
+        serializer=JSONSerializer(),
+        deserializer=JSONDeserializer()
+        )
+    
+    response = predictor.predict(input_data)
+    print(f"Response: {response}")
+    predictor.delete_endpoint()
+
+def prediction_with_boto3(input_data, endpoint_name):
+    print("Predicting with Boto3...")
+    
+    client = boto3.client('sagemaker-runtime')
+
+    payload = json.dumps(input_data)
+
+    response = client.invoke_endpoint(
+        EndpointName=endpoint_name,
+        ContentType='application/json',
+        Body=payload
+    )
+
+    result_body = response['Body'].read().decode('utf-8')
+    result = json.loads(result_body)
+
+    print(f"Response: {result}")    
+
+
+def main():
+    input_data = [17, 5, -40]
+    endpoint_name = "pytorch-inference-2026-04-13-18-28-11-200"
+
+    start = time.time()
+    prediction_with_sagemaker(input_data, endpoint_name)
+    print(f"Time: {time.time() - start}")
+
+    # start = time.time()
+    # prediction_with_boto3(input_data, endpoint_name)
+    # print(f"Time: {time.time() - start}")
+
+if __name__ == "__main__":
+    print("Starting...")
+    main()
+    print("Goodbye!")
